e1470eed89c9dcc02ab9b342e2cec238f70ae5fa
Eunyeol <ney9083@gmail.com>
2022-06-07 21:39:39 +0900

    Feat: 초기 세팅 및 addMenuName 함수 구현

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,3 +1,42 @@
-import '../css/index.css';
+import { $, $$ } from "./utils/DOM";
+import menuItemTemplate from "./utils/MenuItem";
 
-console.log('hello');
+interface Menu {
+  id: Number;
+  soldOut: Boolean;
+  name: String;
+}
+export default function App(this: any) {
+  this.currentCategory = "espresso";
+  this.menu = { [this.currentCategory]: [] };
+  this.init = () => {
+    initEventListener();
+  };
+  this.menuInput = $("#menu-name") as HTMLInputElement;
+  this.menuForm = $("#menu-form") as HTMLFormElement;
+
+  const addMenuName = () => {
+    const newMenuName = this.menuInput.value.trim();
+    this.menuInput.value = "";
+    if (!newMenuName) return;
+    const newMenuObj: Menu = {
+      name: newMenuName,
+      id: Date.now(),
+      soldOut: false,
+    };
+    this.menu[this.currentCategory].push(newMenuObj);
+    console.log(this.menu);
+  };
+
+  const initEventListener = () => {
+    this.menuForm.addEventListener("submit", (e: Event) => {
+      e.preventDefault();
+      addMenuName();
+    });
+  };
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const app = new (App as any)();
+  app.init();
+});
---
+++ src/ts/utils/MenuItem.ts
@@ -0,0 +1,36 @@
+interface Item {
+  id: Number;
+  isSoldOut: Boolean;
+  name: string;
+}
+
+const menuItemTemplate = (item: Item) => {
+  return `
+    <li 
+      class="menu-list-item d-flex items-center py-2" data-menu-id=${item.id}>
+      <span class="${item.isSoldOut ? "sold-out" : ""}  w-100 pl-2 menu-name">${
+    item.name
+  }</span>
+      <button
+        type="button"
+        class="bg-gray-50 text-gray-500 text-sm mr-1 menu-sold-out-button"
+      >
+        품절
+      </button>
+      <button
+        type="button"
+        class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+      >
+        수정
+      </button>
+      <button
+        type="button"
+        class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+      >
+        삭제
+      </button>
+    </li>
+        `;
+};
+
+export default menuItemTemplate;
