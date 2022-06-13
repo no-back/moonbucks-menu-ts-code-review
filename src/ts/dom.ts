d0d7e42ddf29680ea5756f2daf2a2d03d590be05
realhee <wlsgml5628@gmail.com>
2022-06-13 17:15:51 +0900

    Refactor: categoryName에 타입 단언 구문 추가

--- src/ts/dom.ts
+++ src/ts/dom.ts
@@ -7,6 +7,6 @@ export const DOM = {
   $submitButton: $("#espresso-menu-submit-button"),
   $counter: $(".menu-count"),
   $categoryNav: $("#cafe-category-nav"),
-  $categoryName: $(".cafe-category-name"),
+  $categoryName: $(".cafe-category-name") as HTMLElement,
   $menuTitle: $(".mt-1"),
 };
