4984a7cbc68cee425ca4026b95f4b3839dfd79c8
Eunyeol <ney9083@gmail.com>
2022-06-19 20:23:50 +0900

    Refactor: const 분리

--- src/ts/utils/const.ts
+++ src/ts/utils/const.ts
@@ -1,6 +1,32 @@
-export const MESSAGE = {
-  EDIT_MENU: "메뉴명을 수정하세요.",
-  CHECK_DELETE: "정말 삭제하시겠습니까?",
+interface menu {
+  menuName: string;
+  soldOut: boolean;
+  menuId: number;
+}
+
+interface MenuState {
+  [key: string]: menu[];
+}
+
+export const MESSAGE = Object.freeze({
+  EDIT: "메뉴명을 수정하세요.",
+  DELETE: "정말 삭제하시겠습니까?",
   WARN_BLANK: "메뉴를 입력해주세요.",
   ALREADY_EXISTS: "이미 동일한 메뉴가 있습니다.",
-};
+});
+
+export const CATEGORY_STATE: MenuState = Object.freeze({
+  espresso: [],
+  frappucino: [],
+  blended: [],
+  teavana: [],
+  dessert: [],
+});
+
+export const CATEGORY_NAME = Object.freeze({
+  espresso: "☕ 에스프레소",
+  frappuccino: "🥤 프라푸치노",
+  blended: "🍹 블렌디드",
+  teavana: "🫖 티바나",
+  dessert: "🍰 디저트",
+});
